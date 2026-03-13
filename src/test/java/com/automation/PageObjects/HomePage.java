b4d4c6a8122739c038ba71a6f4c63ae734cfc449
nishantbetter-gif <nishantbetter@gmail.com>
2026-03-14 05:06:56 +0530

    Update POM

--- src/test/java/com/automation/PageObjects/HomePage.java
+++ src/test/java/com/automation/PageObjects/HomePage.java
@@ -3,6 +3,7 @@ package com.automation.PageObjects;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -40,10 +41,13 @@ public class HomePage {
     	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
     	WebElement addRemoveLink = wait.until(
-    	        ExpectedConditions.visibilityOf(AddElementLink)
-    	        );
+    	        ExpectedConditions.visibilityOfElementLocated(
+    	                By.xpath("//a[@href='/add_remove_elements/']")
+    	        )
+    	);
 
-    	addRemoveLink.click();
+    	JavascriptExecutor js = (JavascriptExecutor) driver;
+    	js.executeScript("arguments[0].click();", addRemoveLink);
     }
 
     @FindBy(xpath="//a[@href='/dropdown']")
